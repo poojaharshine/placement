01a15b609f06f33d0faeb29769fbfb36949084fe
poojaharshine <poojaharshine03@gmail.com>
2025-08-29 11:45:35 +0530

    Create 1_to_n.java

---
+++ 1_to_n.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        int n;
+        Scanner sc= new Scanner(System.in);
+        n=sc.nextInt();
+        for(int i=1;i<=n;i++){
+            System.out.print(i +" ");
+        }
+        sc.close();
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+    }
+}
